dddaff933056371e5fb43a0c22f30412231693a5
murilo <muril0.esteves@protonmail.com>
2022-12-27 16:41:29 -0400

    implementando avaliação física

--- sistema_habilita/testes_fisicos.py
+++ sistema_habilita/testes_fisicos.py
@@ -25,7 +25,7 @@ def mobilidade_ombro(distancia_entre_maos, tamanho_mao):
         mobilidade = "normal"
     else:
         mobilidade = "reduzida"
-    return mobilidade
+    return mobilidade, referencia_ombro
 
 def mobilidade_quadril(graus_flexão):
     referencia_quadril = 65
@@ -46,7 +46,7 @@ def mobilidade_tornozelo(graus_dorsiflexao):
 def resistencia_isometrica_core (tempo):
     if (tempo <= 20):
         score = "fraco"
-    elif(tempo < 40):
+    elif (tempo < 40):
         score = "regular"
     elif (tempo < 60):
         score = "bom"
@@ -55,5 +55,57 @@ def resistencia_isometrica_core (tempo):
     else:
         score = "excelente"
     return score
+
+def classifica_status_forca(forca_relativa, lista_score):
+    score_final = "nada"
+    def converte_status_forca(score_parcial):
+            score_final = "nada"
+            if (score_parcial == 0):
+                score_final = "Iniciante."
+            elif (score_parcial == 1):
+                score_final = "Antermediário."
+            elif (score_parcial == 2):
+                score_final = "Avançado."
+            else:
+                score_final = "Altamente avançado."
+            return score_final
+
+    index = 0
+    score_parcial = 0
+    for score in lista_score:
+        print("Score: {} Força relativa: {}".format(score, forca_relativa))
+        if (forca_relativa >= score):
+            score_parcial = index
+        index = index + 1
+    return converte_status_forca(score_parcial)
+
+
+
+
+
+
+
+
+
+
+
+
+#supino_homem = [0.6,1,1.2]
+#barra_homem = [0.5, 1, 1.15, 1.3]
+#agachamento_homem = [0.8, 1.2, 1.5]
+#terra_homem = [1, 1.5, 1.8]
+
+#supino_mulher = [0.4, 0.6, 0.8]
+#barra_mulher = [0.5, 1, 1.1]
+#agachamento_mulher = [0.6, 1, 1.3]
+#terra_mulher = [0.8, 1.2, 1.6]
+
+
+
+
+
+
+
+
 
 
