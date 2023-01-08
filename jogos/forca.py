959f327fdc672cca887a75706de6a68c04ad01ff
murilo <muril0.esteves@protonmail.com>
2023-01-08 13:13:19 -0400

    adicinando arquivo com novas palavras e função que lê esse arquivo

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,46 +1,53 @@
+import random
 def jogar():
     print("**************************************")
     print("** Seja bem vindo ao jogo de forca! **")
     print("**************************************")
 
-    palavra_secreta = "banana".upper()
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
-    #listas (mutável) -> nome = [elemento]
-    #truplas(imutável) -> nome = (elemento)
-    #lista de truplas
-    #trupla1 = (2,3)
-    #truplap2 = (3,4)
-    #trupla3 = (5,6)
-    #linha = [trupla1, trupla2, trupla3]
-    #transformar lista em trupla -> nome_variável = tuple.(lista)
-    #trnasformar trupla em lista -> nome_variavel = list(tuple)
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
     erros = 0
 
-    while(not enforcou and not acertou): #game-loop
-        #tratando entrada do usuário
-        chute = (input("Qual letra? "))
-        chute = chute.strip().upper()#devolve a variável sem espaço / letras maiúsculas
+    print(letras_acertadas)
 
-        if(chute in palavra_secreta):
+    while (not enforcou and not acertou):
+
+        chute = (input("Qual letra? "))
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
             index = 0
-            for letra in palavra_secreta: #elemento in lista
-                if(chute == letra):#trata para diferenças entre letras maiúsculas e minúsculas
-                    # print("Econtrei a letra {} na posição {}".format(letra, index))
+            for letra in palavra_secreta:
+                if (chute == letra):
                     letras_acertadas[index] = letra
-                index += 1 #incrementando
+                index += 1
         else:
             erros += 1
 
         enforcou = erros == 6
-        acertou = "_" not in letras_acertadas #não acertou a palavra enquanto houver esse elmento dentro da lista de letras acertadas
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
-    if(acertou):
+
+    if (acertou):
         print("Você ganhou!")
     else:
         print("Você perdeu!")
     print("Fim de jogo!")
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     jogar()
